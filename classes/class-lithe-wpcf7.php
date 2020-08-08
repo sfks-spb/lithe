3617230d870a7eb6caaa449f80f7250667538df0
denpamusic <denpa@denpa.pro>
2020-08-08 08:06:39 +0300

    Add meta and utm form tags

--- classes/class-lithe-wpcf7.php
+++ classes/class-lithe-wpcf7.php
@@ -20,7 +20,152 @@ if ( ! class_exists( 'Lithe_WPCF7' ) ) {
             add_filter( 'wpcf7_form_elements', 'do_shortcode' );
             add_filter( 'wpcf7_display_message', array( $this, 'add_icons_for_message' ), 10, 2 );
             add_filter( 'wpcf7_form_action_url', array( $this, 'save_current_form_config' ) );
+            add_filter( 'wpcf7_posted_data', array( $this, 'handle_meta_posted_data' ), 5 );
+            add_filter( 'wpcf7_posted_data', array( $this, 'handle_utm_posted_data' ), 6 );
+            add_action( 'wpcf7_init', array( $this, 'add_custom_form_tags' ) );
+            add_action( 'wpcf7_after_save', array( $this, 'save_form_meta_fields' ), 10, 1 );
             add_action( 'wp_print_footer_scripts', array( $this, 'add_config_data_storage' ), 8 );
+        }
+
+        /**
+         * Adds custom form tags.
+         *
+         * @return void
+         */
+        public function add_custom_form_tags(): void {
+            wpcf7_add_form_tag(
+                    'meta',
+                    array( $this, 'meta_form_tag_handler' ),
+                    array( 'name-attr' => true ) );
+
+            wpcf7_add_form_tag(
+                    'utm',
+                    array( $this, 'utm_form_tag_handler' ) );
+        }
+
+        /**
+         * Handles meta form tag HTML output.
+         *
+         * @param  WPCF7_FormTag $tag Custom tag instance.
+         *
+         * @return string
+         */
+        public function meta_form_tag_handler( WPCF7_FormTag $tag ): string {
+
+            $atts = array(
+                'type'  => 'hidden',
+                'name'  => 'form_meta[]',
+                'value' => $tag->name,
+            );
+
+            return sprintf( '<input %s />', wpcf7_format_atts( $atts ) );
+        }
+
+        /**
+         * Handles utm form tag HTML output.
+         *
+         * @param  WPCF7_FormTag $tag Custom tag instance.
+         *
+         * @return string
+         */
+        public function utm_form_tag_handler( WPCF7_FormTag $tag ): string {
+
+            $allowed_opts = array(
+                'utm_source',
+                'utm_medium',
+                'utm_campaign',
+            );
+
+            $atts_list = array();
+
+            foreach ( $tag->options as $option ) {
+
+                $option_parts = explode( ':', $option, 2 );
+
+                $name = $option_parts[0];
+
+                if ( ! in_array( $name, $allowed_opts ) || ! isset( $option_parts[1] ) ) continue;
+
+                $atts_list[] = array(
+                    'type'  => 'hidden',
+                    'name'  => 'utm_' . $name,
+                    'value' => $option_parts[1],
+                );
+
+            }
+
+            $inputs = array();
+
+            foreach ( $atts_list as $atts ) {
+                $inputs[] = sprintf( '<input %s />', wpcf7_format_atts( $atts ) );
+            }
+
+            return implode( "\n", $inputs );
+        }
+
+        /**
+         * Injects meta values into the posted data.
+         *
+         * @param  array $posted_data Data posted via form.
+         *
+         * @return array
+         */
+        public function handle_meta_posted_data( array $posted_data ): array {
+
+            if ( ! array_key_exists( 'form_meta', $posted_data ) ) {
+                return $posted_data;
+            }
+
+            $form = WPCF7_ContactForm::get_current();
+            $form_meta = get_post_meta( $form->id(), '_lithe_wpcf7_form_meta', true );
+
+            if ( ! empty ( $form_meta ) ) {
+
+                foreach ( $posted_data['form_meta'] as $name ) {
+
+                    if ( array_key_exists( $name, $form_meta ) ) {
+                        $posted_data[ $name ] = $form_meta[ $name ];
+                    }
+
+                }
+
+            }
+
+            unset( $posted_data['form_meta'] );
+
+            return $posted_data;
+
+        }
+
+        /**
+         * Places utm query string into the posted data.
+         *
+         * @param  array $posted_data Data posted via form.
+         *
+         * @return array
+         */
+        public function handle_utm_posted_data( array $posted_data ): array {
+
+            $allowed_opts = array(
+                'utm_source',
+                'utm_medium',
+                'utm_campaign',
+            );
+
+            $query = array();
+
+            foreach ( $allowed_opts as $opt ) {
+                if ( array_key_exists( $opt, $posted_data ) ) {
+                    $query[ $opt ] = $posted_data[ $opt ];
+                }
+            }
+
+            if ( ! empty( $query ) ) {
+                $posted_data['utm_query'] = esc_attr( http_build_query( $query ) );
+            }
+
+            return $posted_data;
+
         }
 
         /**
@@ -33,16 +178,13 @@ if ( ! class_exists( 'Lithe_WPCF7' ) ) {
 
             $form = WPCF7_ContactForm::get_current();
 
-            $unit_tag = $this->unit_tag_from_url( $url );
-            $postID = (int) ltrim( $unit_tag[2], 'p' );
-
-            $is_registration_form = get_post_meta( $postID, 'registration_form', true );
-
             if ( ! empty( $form ) ) {
+
+                $form_meta = get_post_meta( $form->id(), '_lithe_wpcf7_form_meta', true );
 
                 $this->forms[ $form->id() ] = array(
                     'title'              => $form->title(),
-                    'isRegistrationForm' => ! empty( $is_registration_form ),
+                    'isRegistrationForm' => array_key_exists( 'reg', $form_meta ),
                 );
 
             }
@@ -68,6 +210,34 @@ if ( ! class_exists( 'Lithe_WPCF7' ) ) {
         }
 
         /**
+         * Saves stored meta fields.
+         *
+         * @param  WPCF7_ContactForm $instance Current contact form instance.
+         *
+         * @return void
+         */
+        public function save_form_meta_fields( WPCF7_ContactForm $form ): void {
+
+            $form_meta = array();
+
+            $meta_tags = $form->scan_form_tags( array( 'type' => 'meta' ) );
+
+            if ( ! empty( $meta_tags ) ) {
+
+                foreach ( $meta_tags as $tag ) {
+
+                    $form_meta[ $tag->name ] = isset( $tag->values[0] ) ?
+                        $tag->values[0] : '';
+
+                }
+
+            }
+
+            update_post_meta( $form->id(), '_lithe_wpcf7_form_meta', $form_meta );
+
+        }
+
+        /**
          * Adds FontAwesome icon to form responses.
          *
          * @param  string $message Message content.
@@ -89,18 +259,6 @@ if ( ! class_exists( 'Lithe_WPCF7' ) ) {
             }
 
             return $message;
-        }
-
-        /**
-         * Constructs unit tag array from action url.
-         *
-         * @param  string $url WPCF7 action url.
-         *
-         * @return array
-         */
-        protected function unit_tag_from_url( string $url ): array {
-            $url_parts = explode( '#', $url, 2 );
-            return explode( '-', $url_parts[1] );
         }
 
     }
