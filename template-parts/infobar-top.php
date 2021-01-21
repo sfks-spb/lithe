b69c42f0db510eee618179d8913179b6a9a81e28
denpamusic <denpa@denpa.pro>
2021-01-21 03:45:47 +0300

    Improve infobar style

--- template-parts/infobar-top.php
+++ template-parts/infobar-top.php
@@ -2,22 +2,25 @@
     $message = get_theme_mod( 'lithe_infobar_message', '' );
 
     if ( '' !== $message ):
+
+        $url = get_theme_mod( 'lithe_infobar_link_url', '' );
+        $icon = get_theme_mod( 'lithe_infobar_icon_name', '' );
 ?>
 
-<div class="infobar infobar-top text-align-center" role="alert" style="background-color: <?php echo esc_attr( get_theme_mod( 'lithe_infobar_bg_color', '#105991' ) ); ?>;">
+<div class="infobar infobar-top text-align-center<?php echo ( '' === $url ) ? '' : ' has-cta-button'; echo ( '' === $icon ) ? '' : ' has-icon'; ?>"
+    role="alert"
+    style="background-color: <?php echo esc_attr( get_theme_mod( 'lithe_infobar_bg_color', '#105991' ) ); ?>;">
 
     <div class="infobar-wrapper">
 
-        <?php
-            $icon = get_theme_mod( 'lithe_infobar_icon_name', '' );
+        <?php if ( '' !== $icon ): ?>
 
-            if ( '' !== $icon ):
-        ?>
             <span class="infobar-icon">
 
                 <i class="<?php echo esc_attr( get_theme_mod( 'lithe_infobar_icon_collection', 'fas' ) ); ?> fa-<?php echo esc_attr( $icon ); ?>"></i>
 
             </span>
+
         <?php endif; ?>
 
         <span class="infobar-content">
@@ -26,11 +29,8 @@
 
         </span>
 
-        <?php
-            $url = get_theme_mod( 'lithe_infobar_link_url', '' );
+        <?php if ( '' !== $url ): ?>
 
-            if ( '' !== $url ):
-        ?>
             <span class="infobar-link awaits-icons zooms-in">
 
                 <a title="<?php echo esc_attr( get_theme_mod( 'lithe_infobar_link_title', '' ) ) ?>" href="<?php echo esc_attr( $url ); ?>">
